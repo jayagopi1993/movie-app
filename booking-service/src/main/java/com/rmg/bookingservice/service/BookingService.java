8ad51a801768972eff053ee3e9384c7b0a6ac4f9
Gopinath Rm <jayagopi1993@gmail.com>
2022-04-12 21:10:23 +0530

    Config Server Implemented & refered from different micro service

--- booking-service/src/main/java/com/rmg/bookingservice/service/BookingService.java
+++ booking-service/src/main/java/com/rmg/bookingservice/service/BookingService.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class BookingService {
